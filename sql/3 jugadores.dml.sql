d7c8bc5fa814362b04137b3c1036fe14885d1629
Leandro <leandroq86@gmail.com>
2016-06-05 15:55:42 -0300

    Script SQL - más Jugadores
    Se agregaron inserts para 12 jugadores. Para usarlos en los grupos.

--- sql/3 jugadores.dml.sql
+++ sql/3 jugadores.dml.sql
@@ -1,6 +1,41 @@
 insert into jugadores (nombre, apodo, email, idCategoria, password) 
 values ('Pablo', 'Pablo', 'pablo.carle@gmail.com', 1, '123456');
 
+insert into jugadores (nombre, apodo, email, idCategoria, password) 
+values ('Leandro', 'Leandro', 'Leandro@gmail.com', 1, '123456');
+
+insert into jugadores (nombre, apodo, email, idCategoria, password) 
+values ('Daniel', 'Daniel', 'Daniel@gmail.com', 1, '123456');
+
+
+insert into jugadores (nombre, apodo, email, idCategoria, password) 
+values ('Damian', 'Damian', 'Damian@gmail.com', 1, '123456');
+
+insert into jugadores (nombre, apodo, email, idCategoria, password) 
+values ('Nicolas', 'Nicolas', 'Nicolas@gmail.com', 1, '123456');
+
+
+insert into jugadores (nombre, apodo, email, idCategoria, password) 
+values ('Facundo', 'Facundo', 'Facundo@gmail.com', 1, '123456');
+
+insert into jugadores (nombre, apodo, email, idCategoria, password) 
+values ('Matias', 'Matias', 'matias@gmail.com', 1, '123456');
+
+insert into jugadores (nombre, apodo, email, idCategoria, password) 
+values ('Alejandro', 'Alejandro', 'Alejandro@gmail.com', 1, '123456');
+
+insert into jugadores (nombre, apodo, email, idCategoria, password) 
+values ('Francisco', 'Francisco', 'Francisco@gmail.com', 1, '123456');
+
+insert into jugadores (nombre, apodo, email, idCategoria, password) 
+values ('Ignacio', 'Ignacio', 'Ignacio@gmail.com', 1, '123456');
+
+insert into jugadores (nombre, apodo, email, idCategoria, password) 
+values ('Alexis', 'Alexis', 'Alexis@gmail.com', 1, '123456');
+
+insert into jugadores (nombre, apodo, email, idCategoria, password) 
+values ('Carlos', 'Carlos', 'Carlos@gmail.com', 1, '123456');
+/*
 select * from jugadores;
 
 select * from categorias;
@@ -12,3 +47,5 @@ count(*) as cantidad_jugados,
 sum(case when victoria = 1 then 1 else 0 END CASE;)
 from juego_log
 group by idJugador;
+
+*/
