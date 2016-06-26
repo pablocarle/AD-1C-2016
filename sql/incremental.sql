a1aa98a50887b8154e9629443034122dccb84ec4
pablocarle <pablo.carle@gmail.com>
2016-06-26 17:38:28 -0300

    SQL incremental (posterior a todo otro script)

--- sql/incremental.sql
+++ sql/incremental.sql
@@ -1,5 +1,7 @@
--- Incremental
+-- Incremental (posterior a todos los otros scripts)
 
 update tipo_envites set enviteAnterior = null where enviteAnterior = -1;
 
 alter table envites_pareja_mano modify column aceptado bit null;
+
+alter table bazas modify column idBaza int not null auto_increment;
